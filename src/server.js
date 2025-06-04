4c6aed6e87289f60ab06a5ab07eaf48279fa34fc
Liam kelleher <91279431+Liamkelleher@users.noreply.github.com>
2025-06-03 22:59:50 -0400

    Update server.js

--- src/server.js
+++ src/server.js
@@ -117,11 +117,11 @@ app.post(
 
 // === Route fallback ===
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
+  res.sendFile(path.join(__dirname, "public/index.html"));
 });
 
 app.get("/success", (req, res) => {
-  res.sendFile(path.join(__dirname, "success.html"));
+  res.sendFile(path.join(__dirname, "public/success.html"));
 });
 
 // === Start Server ===
